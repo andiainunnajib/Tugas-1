359617e33fc56ffe330fdf3409a709ed44135210
andiainunnajib <andiainunajib59@gmail.com>
2021-06-07 11:20:47 +0700

    data baru

---
+++ Soal-2.py
@@ -0,0 +1,12 @@
+# Program mengitung luas lingkaran
+
+#input jari jari lingkaran
+jari = int(input("Masukkan jari - jari lingkaran : "))
+pi = 22/7
+luas_lingkaran = pi*jari**2
+
+
+
+
+#output
+print("Luas lingkaran dengan jari-jari", jari, "adalah", luas_lingkaran, "cm\u00b2.")
