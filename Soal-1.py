0c843aaf85ba7ecd5b3bc0cc14cd904fe2a332d2
andiainunnajib <andiainunajib59@gmail.com>
2021-06-07 10:58:47 +0700

    push

---
+++ Soal-1.py
@@ -0,0 +1,9 @@
+Nama = "Andi Ainun Najib"
+Umur = 21
+Tinggi = 172.5
+
+print(type(Nama))
+print(type(Umur))
+print(type(Tinggi))
+
+print("Nama saya ", Nama, "Umur saya ", Umur, "tahun, dan tinggi saya", Tinggi, "cm")
