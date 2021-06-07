121b9692495d19e21a39ea2a1868e031b362a11b
andiainunnajib <andiainunajib59@gmail.com>
2021-06-07 11:20:33 +0700

    Data terbaru

---
+++ Soal-2a.py
@@ -0,0 +1,9 @@
+#definisikan
+def luas_lingkaran(r):
+    return r
+
+r  = int(input("Masukkan nilai jari-jari: "))
+pi = 22/7
+luas_lingkaran = pi * r * r
+
+print("Luas lingkaran dengan jari-jari {} adalah luas_lingkaran {} cm\u00b2.".format(r,luas_lingkaran))
